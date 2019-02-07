56cfbd92b423a3cb57b921355e8bbf9c87a2fe04
ABorgesT <a.t.borgao@gmail.com>
2019-02-07 16:34:33 +0000

    Idk

---
+++ CrossJam_50Years/Assets/Scripts/LevelSelector_Scripts/LevelButtons.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelButtons : MonoBehaviour {
+
+    public GameObject elevatorPanels;
+    
+    public void LevelSelector()
+    {
+        elevatorPanels.SetActive(true);
+
+    }
+}
