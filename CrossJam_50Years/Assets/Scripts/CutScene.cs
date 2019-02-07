469dd48978407923d4a33649a8fe911e8cc6bac8
GRCP52 <goncalorcperes@hotmail.com>
2019-02-07 21:17:39 +0000

    Be

---
+++ CrossJam_50Years/Assets/Scripts/CutScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutScene : MonoBehaviour
+{
+
+    public Transform spot;
+    public Rigidbody2D thePlayer;
+
+    private void Update()
+    {
+        stopMoving();
+    }
+
+    void stopMoving ()
+    {
+        if (spot.position.x <= thePlayer.position.x)
+            thePlayer.GetComponent<PlayerMovement>().runSpeed = 0;
+    }
+
+}
