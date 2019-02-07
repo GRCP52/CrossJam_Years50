0bda475653709b7fa7d5707b61e9355e9e131a89
ABorgesT <a.t.borgao@gmail.com>
2019-02-07 21:18:41 +0000

    All mechanics almost done

---
+++ CrossJam_50Years/Assets/Scripts/F_Controller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class F_Controller : MonoBehaviour {
+
+    public float speed;
+
+    public Transform spot;
+
+    private void Start()
+    {
+        StartCoroutine("Moving");
+    }
+
+    private void LateUpdate()
+    {
+        if (transform.position.x == spot.position.x)
+            Destroy(gameObject);
+    }
+
+    IEnumerator Moving() {
+        while (transform.position.x != spot.position.x && transform.position.y != spot.position.y)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(spot.position.x, transform.position.y), speed);
+
+            yield return null;
+        }
+    }
+}
