68d04015734e5df54c33d5f6fc9edfb2a748b8dc
GRCP52 <goncalorcperes@hotmail.com>
2019-02-07 13:25:47 +0000

    A lot

---
+++ CrossJam_50Years/Assets/Scripts/Player_Scripts/PlayerMovementJon.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementJon : MonoBehaviour
+{
+
+    public PlayerController2D controller;
+
+    public float horizontalMove = 0f;
+
+    public float runSpeed = 40f;
+
+    public bool jump = false;
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetKeyDown("w") | Input.GetKeyDown("space"))
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
