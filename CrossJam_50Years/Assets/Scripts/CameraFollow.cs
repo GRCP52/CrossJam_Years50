032aad4b4b22e5339de438dec134700a3aa3c4b4
GRCP52 <goncalorcperes@hotmail.com>
2019-02-07 17:03:35 +0000

    better camera follow

--- CrossJam_50Years/Assets/Scripts/CameraFollow.cs
+++ CrossJam_50Years/Assets/Scripts/CameraFollow.cs
@@ -8,12 +8,7 @@ public class CameraFollow : MonoBehaviour
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
 
-    private void Start()
-    {
-        transform.position = new Vector3(transform.position.x, 0, 0);
-    }
-
-    private void FixedUpdate()
+    private void Update()
     {
 
         transform.position = new Vector3((target.position.x + offset.x), (0 + offset.y), (target.position.z + offset.z));
