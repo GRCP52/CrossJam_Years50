f300e354cee7f7693ff8665e9b20846b7d842a39
GRCP52 <goncalorcperes@hotmail.com>
2019-02-07 16:34:57 +0000

    Camera Follow

---
+++ CrossJam_50Years/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    private void Start()
+    {
+        transform.position = new Vector3(transform.position.x, 0, 0);
+    }
+
+    private void FixedUpdate()
+    {
+
+        transform.position = new Vector3((target.position.x + offset.x), (0 + offset.y), (target.position.z + offset.z));
+
+    }
+}
