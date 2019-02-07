14a005978895c58ab19e5e0a541096ace20dbb21
GRCP52 <goncalorcperes@hotmail.com>
2019-02-07 18:29:51 +0000

    b

---
+++ CrossJam_50Years/Assets/Scripts/Player_Scripts/ObstacleAssignment.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleAssignment : MonoBehaviour
+{
+
+    public GameObject thePlayer;
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+
+        Destroy(col.gameObject);
+        thePlayer.GetComponent<PlayerMovement>().runSpeed *= 0.90f;
+
+    }
+
+}
