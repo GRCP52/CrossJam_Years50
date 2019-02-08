72fcdecde11c797d4aaa53af95dd33ba1df62691
GRCP52 <goncalorcperes@hotmail.com>
2019-02-08 12:38:26 +0000

    Transitions

--- CrossJam_50Years/Assets/Scripts/CutScene.cs
+++ CrossJam_50Years/Assets/Scripts/CutScene.cs
@@ -8,15 +8,20 @@ public class CutScene : MonoBehaviour
     public Transform spot;
     public Rigidbody2D thePlayer;
 
+    public bool playerStoped = false;
+
     private void Update()
     {
-        stopMoving();
+        StopMoving();
     }
 
-    void stopMoving ()
+    void StopMoving ()
     {
         if (spot.position.x <= thePlayer.position.x)
+        {
             thePlayer.GetComponent<PlayerMovement>().runSpeed = 0;
+            playerStoped = true;
+        }
     }
 
 }
