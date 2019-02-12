ed065d0cd3352236e8d98f6f8cc7705cba86e7a6
GRCP52 <goncalorcperes@hotmail.com>
2019-02-12 21:18:03 +0000

    sizes

--- CrossJam_50Years/Assets/Scripts/MainMenu.cs
+++ CrossJam_50Years/Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour {
 
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("Cut_Scene");
     }
 
     public void QuitGame()
